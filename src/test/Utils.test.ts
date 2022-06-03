5b87adf90f0c2f3c646910b595771f9873c0bb7a
nicoledbianchin <nicoledbianchin@gmail.com>
2022-06-03 15:15:30 -0300

    simple assertions

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,8 +2,24 @@ import { Utils } from '../app/Utils';
 
 describe('Utils test suit', () => {
     test('First test', () => {
-        const abc = Utils.toUpperCase('something');
-        console.log('working')
+        const result = Utils.toUpperCase('something');
+        expect(result).toBe('SOMETHING')
     })
 
+    test('Parse simple url', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:4444/login');
+        expect(parsedUrl.href).toBe('http://localhost:4444/login');
+        expect(parsedUrl.port).toBe('4444');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    });
+
+    test('Parse url with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:4444/login?user=user&password=1234');
+        const query = {
+            user: 'user',
+            password: '1234'
+        }
+        expect(parsedUrl.query).toEqual(query);
+    });
 });

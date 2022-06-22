cd21840dfdbabb1a4d24daead1614dcf2f044fcc
nicoledbianchin <nicoledbianchin@gmail.com>
2022-06-22 13:46:03 -0300

    mocks returning values

--- src/test/Handlers/LoginHandler.test.ts
+++ src/test/Handlers/LoginHandler.test.ts
@@ -1,5 +1,7 @@
 import { LoginHandler } from '../../app/Handlers/LoginHandler';
-import { HTTP_CODES, HTTP_METHODS } from '../../app/Models/ServerModels';
+import { HTTP_CODES, HTTP_METHODS, SessionToken } from '../../app/Models/ServerModels';
+import { Utils } from '../../app/Utils/Utils';
+
 
 describe('LoginHandler test suite', () => {
     let loginHandler: LoginHandler;
@@ -7,9 +9,22 @@ describe('LoginHandler test suite', () => {
         method: ''
     };
     const responseMock = {
-        writeHead: jest.fn()
+        writeHead: jest.fn(),
+        write: jest.fn(),
+        statusCode: 0
     };
-    const authorizeMock = {};
+    const authorizeMock = {
+        generateToken: jest.fn()
+    };
+    const getRequestBodyMock = jest.fn();
+
+    const someSessionToken: SessionToken = {
+        tokenId: 'tokenId',
+        userName: 'username',
+        valid: true,
+        expirationTime: new Date(),
+        accessRights: [1, 2, 3]
+    }
 
     beforeEach(() => {
         loginHandler = new LoginHandler(
@@ -17,6 +32,7 @@ describe('LoginHandler test suite', () => {
             responseMock as any,
             authorizeMock as any
         );
+        Utils.getRequestBody = getRequestBodyMock;
     });
 
     afterEach(() => {
@@ -25,14 +41,33 @@ describe('LoginHandler test suite', () => {
 
     test('options request', async () => {
         requestMock.method = HTTP_METHODS.OPTIONS;
+
         await loginHandler.handleRequest();
+
         expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.OK);
         expect(responseMock.writeHead).toBeCalledTimes(1);
     });
 
     test('not handled http method', async () => {
         requestMock.method = 'invalidMethod';
+
         await loginHandler.handleRequest();
+
         expect(responseMock.writeHead).not.toBeCalled();
-    })
+    });
+
+    test('post request with valid login', async () => {
+        requestMock.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockReturnValueOnce({
+            username: 'user',
+            password: 'pass'
+        });
+        authorizeMock.generateToken.mockReturnValueOnce(someSessionToken);
+
+        await loginHandler.handleRequest();
+
+        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, { 'Content-Type': 'application/json' });
+        expect(responseMock.write).toBeCalledWith(JSON.stringify(someSessionToken));
+    });
 })

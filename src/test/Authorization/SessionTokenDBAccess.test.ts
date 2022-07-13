5b5c18065cbcd661fa8446af21d296ff8dca228a
nicoledbianchin <nicoledbianchin@gmail.com>
2022-07-13 10:09:29 -0300

    mockando callbacks

--- src/test/Authorization/SessionTokenDBAccess.test.ts
+++ src/test/Authorization/SessionTokenDBAccess.test.ts
@@ -18,7 +18,9 @@ describe('SessionTokenDBAccess test suite', () => {
         tokenId: '123',
         userName: 'John',
         valid: true
-    }
+    };
+    const someTokenId = '123';
+
     beforeEach(() => {
         sessionTokenDBAccess = new SessionTokenDBAccess(nedbMock as any);
 
@@ -47,5 +49,39 @@ describe('SessionTokenDBAccess test suite', () => {
         )
         await expect(sessionTokenDBAccess.storeSessionToken(someToken)).rejects.toThrow('something went wrong');
         expect(nedbMock.insert).toBeCalledWith(someToken, expect.any(Function));
+    });
+
+    test('get token with result and no error', async () => {
+        const bar = (someTokenId: string, cb: any) => {
+            cb(null, [someToken])
+        }
+        nedbMock.find.mockImplementationOnce(bar);
+        const getTokenResult = await sessionTokenDBAccess.getToken(someTokenId);
+        expect(getTokenResult).toBe(someToken);
+        expect(nedbMock.find).toBeCalledWith({ tokenId: someTokenId }, expect.any(Function));
+    });
+
+    test('get token with no result and no error', async () => {
+        const bar = (someTokenId: string, cb: any) => {
+            cb(null, [])
+        }
+        nedbMock.find.mockImplementationOnce(bar);
+        const getTokenResult = await sessionTokenDBAccess.getToken(someTokenId);
+        expect(getTokenResult).toBeNull;
+        expect(nedbMock.find).toBeCalledWith({ tokenId: someTokenId }, expect.any(Function));
+    });
+
+    test('get token with error', async () => {
+        const bar = (someTokenId: string, cb: any) => {
+            cb(new Error("something went wrong"), [])
+        }
+        nedbMock.find.mockImplementationOnce(bar);
+        await expect(sessionTokenDBAccess.getToken(someTokenId)).rejects.toThrow("something went wrong");
+        expect(nedbMock.find).toBeCalledWith({ tokenId: someTokenId }, expect.any(Function));
+    });
+
+    test('constructor argument', async () => {
+        new SessionTokenDBAccess();
+        expect(Nedb).toBeCalledWith('databases/sessionToken.db')
     });
 });
